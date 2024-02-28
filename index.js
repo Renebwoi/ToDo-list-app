d1db555dc3b0daf763f02be4dac1484d6b9fe79e
Renebwoi <demiurgerene@gmail.com>
2024-02-28 15:15:24 +0100

    added conditional scripts and message item manipultion

--- index.js
+++ index.js
@@ -2,24 +2,73 @@ let itemBox = document.querySelector(".itemBox")
 let addBtn =document.querySelector(".addItem")
 let clearBtn = document.querySelector(".clearButton")
 let textInpt = document.getElementById("searcher")
-
+// let canceller = document.querySelectorAll(".cancelButton")
 // addBtn.onclick = addit()
-console.log("ghe")
+// console.log("ghe")
 
 function addit() {
-    console.log(textInpt.value)
+    // console.log(textInpt.value)
     itemBox.innerHTML += 
     `<article class="item">
     <div class="timeInfo">
-        <div class="date">12/03/24</div>
-        <div class="time">6:34</div>
-        <div class="cancelButton">X</div>
+        <div class="date">${formatDate(new Date())}</div>
+        <div class="time">${formatAMPM(new Date())}</div>
+        <div class="cancelButton" onclick="this.parentElement.parentNode.remove();">X</div>
     </div>
 <hr>
     <div class="info">
         <p class="innerInfo">${textInpt.value}</p>
-        <!-- <p class="message">two tea and 5 lemons</p> -->
+        <!-- <p class="message"  >two tea and 5 lemons</p> -->
     </div>
     
 </article>`
 }
+
+// canceller.addEventListener('click', console.log("wheeee..."))
+
+function cancel() {
+    console.log("heyo")
+    this.parentElement.style.display = 'none'
+}
+
+// canceller.addEventListener('click', function() {
+//     this.closest('.item').style.display = 'none';
+//   });
+
+//   // Attach the event listener to each element
+// canceller.forEach(function(element) {
+//     element.addEventListener('click', myFunction);
+//     console.log("jyu")
+// });
+// function myFunction() {
+//     this.closest('.item').style.display = 'none';
+//     console.log("omeuwe")
+// }
+
+function clearEverything() {
+    itemBox.innerHTML = " "
+}
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    var strTime = hours + ":" + minutes + " " + ampm;
+    return strTime;
+  }
+
+//   console.log(formatAMPM(new Date()));
+
+  function formatDate(date) {
+    var day = date.getDate();
+    var month = date.getMonth() +1;
+    var year = date.getFullYear();
+    
+    var striTime = `${day}/${month}/${year}`;
+    return striTime;
+  }
+
+  console.log("done")
---
+++ seter.js
@@ -0,0 +1,41 @@
+//  g =3
+
+// if(g<5){
+//     document.body.innerHTML += `<script src = "index.js"></script>" `
+// }
+
+//the index is for creating and storing the variables, while script is for fetching the stored data 
+//and using it to represent the data
+
+function loadScript(src, callback) {
+    var newScript = document.createElement('script');
+    newScript.type = 'text/javascript';
+    newScript.async = true;
+    newScript.src = src;
+
+    if (newScript.readyState) {
+        newScript.onreadystatechange = function () {
+            if (/loaded|complete/.test(newScript.readyState)) {
+                callback();
+            }
+        };
+    } else {
+        newScript.addEventListener('load', callback, false);
+    }
+
+    document.documentElement.firstChild.appendChild(newScript);
+}
+
+if (3==3) {
+    loadScript('index.js', function () {
+        console.log("index loaded")
+        // Code to execute after lulz.js is loaded
+        // ...
+    });
+} else {
+    loadScript('script.js', function () {
+        console.log("script loaded")
+        // Code to execute after other_lulz.js is loaded
+        // ...
+    });
+}
